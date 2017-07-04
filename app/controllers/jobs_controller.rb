3a12310fbfe1ea3aefdde1c174226867f373b570
AnattaGuo <mengjunkwok@gmail.com>
2017-07-04 21:09:00 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -23,7 +23,7 @@ class JobsController < ApplicationController
   end
 
   def edit
-    @job = Job.fin(params[:id])
+    @job = Job.find(params[:id])
   end
 
   def update
@@ -45,6 +45,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
